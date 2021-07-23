e417c6874f90a3bf930226e16ff228a4d6d8f81f
johan <johan2.github@gmail.com>
2021-07-23 19:06:37 +0200

    add scraping for currency api

--- server/server.js
+++ server/server.js
@@ -1,13 +1,26 @@
-// var cors = require('cors')
+
 const sequelize = require("./lib/sequelize");
 const express = require("express");
 const cors = require("cors");
+const Currency = require("./models/mongo/Currency");
+
+const { MongooseGenerator, Scrapper } = require("./scrapper");
 
 const app = express();
 const authRouter = require("./routes/auth");
 const adminRouter = require("./routes/admin");
 
 const dotenv = require('dotenv');
+
+var CronJob = require('cron').CronJob;
+var job = new CronJob('0 0 * * * *', function() {
+    new Scrapper(
+        { url: "http://api.exchangeratesapi.io/v1/latest?access_key=52c76c294d6c9b2cbb3dd2a282e28e19" },
+        (data) => data.rates,
+        (data) => MongooseGenerator(data, Currency)
+    ).scrap();
+}, null, true, 'America/Los_Angeles');
+job.start();
 
 dotenv.config();
 app.use(cors()) // Use this after the variable declaration
