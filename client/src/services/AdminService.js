34c7e6b5903de1420f178962e39b038f09c6606f
johan <johan2.github@gmail.com>
2021-07-25 19:30:08 +0200

    reorganize file structure for transactions

---
+++ client/src/services/AdminService.js
@@ -0,0 +1,19 @@
+import http from "../http-common";
+
+const getAll = () => {
+  return http.get("/admin/transaction");
+};
+
+const get = id => {
+  return http.get(`/admin/transaction/${id}`);
+};
+
+
+
+export default {
+  getAll,
+  get,
+  create,
+  update,
+  remove,
+};
